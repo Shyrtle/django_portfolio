0439d5fab49dc22c6e0538292d9555f55a853edc
Shyrtle <joshyxboy@gmail.com>
2018-11-02 20:43:52 -0800

    initial commit

---
+++ jobs/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class Job(models.Model):
+    # Image
+    image = models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length=200)
